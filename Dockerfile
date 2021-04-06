bb7775de1948e6665a73009671cde1aae021caca
Fubu <agutierrez@posteo.net>
2021-04-06 18:30:01 +0200

    Using test csvs
    Also modified Dockerfile to copy that data. Moved app copy to avoid
    rebuilding the full container after every change in the app itself.

--- Dockerfile
+++ Dockerfile
@@ -2,15 +2,22 @@ FROM continuumio/miniconda3
 
 WORKDIR /app
 
-COPY ./app/ .
-
 RUN apt-get -y update && apt-get install -y --no-install-recommends \
         gcc \
         linux-libc-dev \
         libc6-dev
 
+# Copy conda.yaml for installation
+COPY ./app/conda.yaml .
+
 RUN conda env create -f conda.yaml
 
 # Make RUN commands use the new environment:
-SHELL ["conda", "run", "-n", "myenv", "/bin/bash", "-c"]
+SHELL ["conda", "run", "-n", "aisarch_pipeline", "/bin/bash", "-c"]
+
+# Copy application
+COPY ./app/ .
+
+# Copy data
+COPY ./test_data /home/data
 
--- app/test_mlflow.sh
+++ app/test_mlflow.sh
@@ -1,18 +1,8 @@
 #!/bin/sh
 
-# HERMES
-# mlflow run . --no-conda -P ihs_path=file:///home/fubu/data/IHS/ihs_data.csv \
-#     -P ais_path=file:///home/fubu/data/AIS/AIS2016/abel_2016-01.csv \
-#     -P model="STEAM,STEAM2" -P export_db=False -P unit="kg" -P step=60 \
-#     -P \
-#     hermes_file=file:///home/fubu/data/HERMES/day_summary.csv \
-#     -P ae_on_lim=0
-# Remember that there is a --no-conda parameter!
-
-# NO HERMES
-mlflow run . \
-    -P ihs_path=file:///home/agutierrez/data/IHS/2021-02-24-ihs_data_basic.csv \
-    -P ais_path=file:///home/agutierrez/data/AIS/AIS2016/abel_2016-01.csv \
+mlflow run . --no-conda\
+    -P ihs_path=file:///home/data/anon_ihs_test.csv \
+    -P ais_path=file:///home/data/anon_2016-01.csv \
     -P model="STEAM,STEAM2" -P export_db=False -P unit="kg" -P step=60 \
     -P ae_on_lim=0 \
-    -P csv_output=file:///home/agutierrez/data/calc_emis
+    -P csv_output=file:///home/data/calc_emis
