1f8e674cd92701ec69def007dbf7672559d472fc
Fubu <agutierrez@posteo.net>
2021-04-08 12:46:11 +0200

    Renamed test script

---
+++ app/test_femiot.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+mlflow run . --no-conda\
+    -P ihs_path=file:///home/data/femiot/anon_ihs_test.csv \
+    -P ais_path=file:///home/data/femiot/anon_2016-01.csv \
+    -P model="STEAM,STEAM2" -P export_db=False -P unit="kg" -P step=60 \
+    -P ae_on_lim=0 \
+    -P csv_output=file:///home/data/femiot/calc_emis
